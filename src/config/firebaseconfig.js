010ef1a484361216e4d292da6d7bfaf7873ddcbd
Naivedya Tripathi <165983020+naivedya-13@users.noreply.github.com>
2024-05-07 15:33:06 +0530

    Update firebaseconfig.js

--- src/config/firebaseconfig.js
+++ src/config/firebaseconfig.js
@@ -8,28 +8,21 @@ import {getStorage} from "firebase/storage"
 
 
 
-// const firebaseConfig = {
-//     apiKey: process.env.REACT_APP_API_KEY ,
-//     authDomain: process.env.REACT_APP_AUTH_DOMAIN ,
-//     projectId: process.env.REACT_APP_PROJECT_ID ,
-//     storageBucket: process.env.REACT_APP_STORAGE_BUCKET ,
-//     messagingSenderId :process.env.REACT_APP_MESSAGING_SENDER_ID ,
-//     appId: process.env.REACT_APP_APPID,
-//   };
-
-
-
-
-
-
 const firebaseConfig = {
-  apiKey: "AIzaSyB-mm5LX0ujuK7UzMWfDLZ96P3hRGhCGuw",
-  authDomain: "expressume-61b8a.firebaseapp.com",
-  projectId: "expressume-61b8a",
-  storageBucket: "expressume-61b8a.appspot.com",
-  messagingSenderId: "348275949801",
-  appId: "1:348275949801:web:399f95b1ff6a3eef8d3078"
-};
+    apiKey: process.env.REACT_APP_API_KEY ,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN ,
+    projectId: process.env.REACT_APP_PROJECT_ID ,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET ,
+    messagingSenderId :process.env.REACT_APP_MESSAGING_SENDER_ID ,
+    appId: process.env.REACT_APP_APPID,
+  };
+
+
+
+
+
+
+
 
 
 
